cb253b6a860b9df9c029402f985ad65558bf0e36
Ayushi-Kosta <83204540+Ayushi-Kosta@users.noreply.github.com>
2021-12-31 09:58:57 +0530

    Time: 4 ms (51.99%), Space: 6 MB (23.23%) - LeetHub

--- sqrtx/sqrtx.cpp
+++ sqrtx/sqrtx.cpp
@@ -1,31 +1,47 @@
 class Solution {
 public:
     int mySqrt(int x) 
-    {   
-        long int beg = 1;
-        long int end = x;
-        
-        long int res = 0;
-        
-        while(beg <= end)
-        {
-            long long int mid = (beg+end)/2;
-            
-            if(mid * mid == x)
-            {
+    {    long long low = 1, high = x;
+        long long ans = 0;
+        while(low <= high) {
+            long long mid = low + (high-low)/2;
+            if(mid*mid == x) 
                 return mid;
+            else if(mid*mid<x) {
+                ans = mid;
+                low = mid + 1;
             }
-            
-            if(mid * mid < x)
-            { 
-                beg = mid+1;
-                res = mid;
-            }
-            
             else
-                end = mid-1;
+                high = mid - 1;
         }
+        return ans;
         
-        return res;
+        
+        
+//         long int beg = 1;
+//         long int end = x;
+        
+//         long int res = 0;
+        
+//         while(beg <= end)
+//         {
+//             long long int mid = (beg+end)/2;
+            
+//             if(mid * mid == x)
+//             {
+//                 return mid;
+//             }
+            
+//             if(mid * mid < x)
+//             { 
+//                 beg = mid+1;
+//                 res = mid;
+//             }
+            
+//             else
+//                 end = mid-1;
+//         }
+        
+//         return res;
     }
 };
