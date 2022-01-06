b1350748dae08cc34c3dc2a0697a9737333d776e
Ayushi-Kosta <83204540+Ayushi-Kosta@users.noreply.github.com>
2022-01-07 00:25:51 +0530

    Time: 8 ms (95.97%), Space: 10.1 MB (60.37%) - LeetHub

---
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        
+        int n=trips.size();
+        vector<int> a(1005, 0);
+        
+        for(int i=0; i<trips.size(); i++){
+            a[trips[i][1]]+=trips[i][0];
+            a[trips[i][2]]-=trips[i][0];
+        }
+        int c=0;
+        for(int i=0; i<=1000; i++){
+            c+=a[i];
+            if(c>capacity){
+                return 0;
+            }
+        }
+        return 1;
+    }
+};
