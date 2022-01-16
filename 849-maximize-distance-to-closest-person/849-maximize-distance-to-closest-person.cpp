0ef5330d22a4058155d166b4e174c78b318ae17c
Ayushi-Kosta <83204540+Ayushi-Kosta@users.noreply.github.com>
2022-01-16 22:12:10 +0530

    Time: 8 ms (96.68%), Space: 17.1 MB (32.93%) - LeetHub

--- 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
+++ 849-maximize-distance-to-closest-person/849-maximize-distance-to-closest-person.cpp
@@ -1,6 +1,45 @@
 class Solution {
 public:
-    int maxDistToClosest(vector<int>& seats) {
+    int maxDistToClosest(vector<int>& arr) {
+        
+//     int res = INT_MIN , ans = 0, n=arr.size();
+
+//     if(arr[0] == 1){
+
+//     for(int i=1; i < n; i++){
+
+//             if(arr[i] == 1){
+//                 ans = i + 1 - ans;
+//                 res = max(res,ans);
+//             }
+        
+//     }
+
+//     }
+
+    bool f =0;
+    int ans=0, n=arr.size();
+    int ind_1=0, ind_2=0, first_1=INT_MAX, last_1=INT_MIN;
+    for(int i=0; i<n; i++){
+        if(f==0 and arr[i]==1){
+            ind_1=i;
+            f=1;
+        }
+        if(f==1 and arr[i]==1){
+            ind_2=ind_1;
+            ind_1=i;
+            ans=max(ans, abs(ind_2-ind_1)/2);
+        }
+        
+        if(arr[i]==1){
+            first_1=min(first_1, i);
+            last_1=max(last_1, i);
+        }
+    }
+    ans=max(ans, (first_1));
+    ans=max(ans, (n-1-last_1));
+    return ans;
+        
 //         int n=seats.size(), first_1=INT_MAX, last_1=INT_MIN, curr_1=0, curr_1_next=0, ans=0;
 //         bool found_1=0, found_1_next=0;
 //         for(int i=0; i<n; i++){
@@ -25,35 +64,5 @@ public:
 //         ans=max(ans, first_1);
 //         ans=max(ans, n-1-last_1);
 //         return ans;
-        int n = seats.size(), longestEmpty = 1;
-        int len = 0;
-        int first_1=INT_MAX, last_1=INT_MIN;
-        bool found_1=0;
-        for(int i = 0; i<n; i++) {
-            //cout<<len<<" ";
-            if(seats[i] == 0) {
-                len++;
-            }
-            if(seats[i] and len) {
-                longestEmpty = max(longestEmpty, (len+1)/2);
-                len = 0;
-            }
-            if(seats[i]==1 and !found_1){
-                first_1=i;
-                found_1=1;
-            }
-            if(seats[i]==1){
-                last_1=max(last_1, i);
-               
-            }
-        }
-        //cout<<len<<" --- ";
-        longestEmpty = max(longestEmpty, (len+1)/2);
-        //cout<<longestEmpty<<" ";
-        longestEmpty = max(longestEmpty, first_1);
-        //cout<<longestEmpty<<" ";
-        longestEmpty = max(longestEmpty, n-1-last_1);
-        //cout<<longestEmpty<<" ";
-        return (longestEmpty);
     }
 };
