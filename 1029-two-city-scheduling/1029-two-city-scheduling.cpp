c7da76f29d3895514d41068b40fd7756ce26ea03
Ayushi-Kosta <83204540+Ayushi-Kosta@users.noreply.github.com>
2022-01-25 22:53:20 +0530

    Time: 10 ms (30.28%), Space: 7.9 MB (75.64%) - LeetHub

---
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    static bool mycomp(vector<int> &a, vector<int> &b){
+        return (a[0]-a[1])<(b[0]-b[1]);
+    }
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+        vector<int> a, b;
+        int n=costs.size();
+        vector<vector<int>> q;
+        sort(costs.begin(), costs.end(), mycomp);
+        int ans=0, i=0;
+        n=n/2;
+        while(i<n){
+            ans+=(costs[i][0] + costs[i+n][1]);
+                i++;
+        }
+        return ans;
+        
+    }
+};
