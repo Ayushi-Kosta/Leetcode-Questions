98e36f6e0cb97a44a5e5531e7b9c594e018263af
Ayushi-Kosta <83204540+Ayushi-Kosta@users.noreply.github.com>
2022-02-02 12:40:28 +0530

    Time: 131 ms (7.81%), Space: 12.7 MB (13.17%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -1,51 +1,60 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        int n_s=s.size(), n_p=p.size();
-        string a ;
-        vector<int> v;
-        if(n_p > n_s){
-            return v;
-        }
-        unordered_map<char, int> m1, m2;
-        for(int i=0; i<n_p; i++){
-            a+=s[i];
-            m1[s[i]]++;
-            m2[p[i]]++;
-        }
-        bool t=1;
-        for(int j=0; j<n_p; j++){
-            if(m1[p[j]]!=m2[p[j]]){
-                t=0;
-                break;
+        unordered_map<char, int>sm, pm;
+        for(auto &ch: p) pm[ch]++;
+        
+        vector<int> indices;
+        int j = 0, n = s.size(), m = p.size(), perfect = 0;
+        for(int i = 0; i<n; i++) {
+            sm[s[i]]++;
+            if(i-j+1 == m) {
+                if(sm==pm) indices.push_back(j);
+                sm[s[j]]--;
+                if(sm[s[j]] == 0) 
+                    sm.erase(s[j]);
+                j++;
             }
         }
-        if(t){
-            v.push_back(0);
-        }
-        int i=n_p;
-        for(i=n_p; i<n_s; i++){
-            if(m1[s[i-n_p]] == 1){
-                m1.erase(s[i-n_p]);
-            }
-            else{
-                m1[s[i-n_p]]--;
-            }
-            m1[s[i]]++;
-            if(m1 == m2){
-                v.push_back(i-n_p+1);
-            }
-            // bool t=1;
-            // for(int j=0; j<n_p; j++){
-            //     if(m1[p[j]]!=m2[p[j]]){
-            //         t=0;
-            //         break;
-            //     }
-            // }
-            // if(t){
-            //     v.push_back(i-n_p+1);
-            // }
-        }
-        return v;
+        return indices;
     }
+    
+    // vector<int> findAnagrams(string s, string p) {
+    //     int n_s=s.size(), n_p=p.size();
+    //     string a ;
+    //     vector<int> v;
+    //     if(n_p > n_s){
+    //         return v;
+    //     }
+    //     unordered_map<char, int> m1, m2;
+    //     for(int i=0; i<n_p; i++){
+    //         a+=s[i];
+    //         m1[s[i]]++;
+    //         m2[p[i]]++;
+    //     }
+    //     bool t=1;
+    //     for(int j=0; j<n_p; j++){
+    //         if(m1[p[j]]!=m2[p[j]]){
+    //             t=0;
+    //             break;
+    //         }
+    //     }
+    //     if(t){
+    //         v.push_back(0);
+    //     }
+    //     int i=n_p;
+    //     for(i=n_p; i<n_s; i++){
+    //         if(m1[s[i-n_p]] == 1){
+    //             m1.erase(s[i-n_p]);
+    //         }
+    //         else{
+    //             m1[s[i-n_p]]--;
+    //         }
+    //         m1[s[i]]++;
+    //         if(m1 == m2){
+    //             v.push_back(i-n_p+1);
+    //         }
+    //     }
+    //     return v;
+    // }
 };
