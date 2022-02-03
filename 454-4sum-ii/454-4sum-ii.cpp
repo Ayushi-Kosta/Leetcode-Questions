7d9c242c965169962147cf2656b8477718dfd45f
Ayushi-Kosta <83204540+Ayushi-Kosta@users.noreply.github.com>
2022-02-03 08:50:28 +0530

    Time: 339 ms (41.26%), Space: 24.3 MB (94.70%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.cpp
+++ 454-4sum-ii/454-4sum-ii.cpp
@@ -2,13 +2,17 @@ class Solution {
 public:
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
         int n = nums1.size();
-        int i=0, j=0, k=0, l=0, count=0;
+        int count=0;
         unordered_map<int, int> m;
+        
+        //storing every possible value of sum of elements of nums1 and nums2 and its occurence
         for(int z=0; z<n; z++){
             for(int x=0; x<n; x++){
                 m[nums1[z]+nums2[x]]++;
             }
         }
+        
+        //checking for every possible value of sum of elements of nums3 and nums4 and adding its occurence in count                 variable
         for(int z=0; z<n; z++){
             for(int x=0; x<n; x++){
                 if(m.find(-(nums3[z]+nums4[x])) != m.end()){
@@ -16,6 +20,7 @@ public:
                 }
             }
         }
+        
         return count;
     }
 };
