a264223b149dbb2ada15d5ba44be4bbaea9877b3
Ayushi-Kosta <83204540+Ayushi-Kosta@users.noreply.github.com>
2022-02-10 13:14:29 +0530

    Time: 3 ms (91.92%), Space: 6.7 MB (56.82%) - LeetHub

---
+++ 38-count-and-say/38-count-and-say.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    string dp[31];
+    void rec(int n){
+        if(n==0){
+            
+            return;
+        }
+        if(n==1){
+            dp[1]="1";
+            return ;
+        }
+        if(n==2){
+            dp[2]="11";
+            return;
+        }
+        else{
+            rec(n-1);
+            
+            string s="";
+        int a=dp[n-1].size(), count=1, i=1;
+        while(i<a){
+            while(dp[n-1][i-1]==dp[n-1][i]){
+                count++;
+                i++;
+            }
+            s+=(count+'0');
+            s+=(dp[n-1][i-1]);
+            count=0;
+        }
+        dp[n]=s;    
+            
+        }
+        
+        
+    }
+    string countAndSay(int n) {
+        string ans="1";
+        
+            while(--n){
+                string s="";
+                int z=ans.size();
+                for(int i=0; i<z; i++){
+                    int count=1;
+                    while(i+1<z and ans[i]==ans[i+1]){
+                        count++;
+                        i++;
+                    }
+                    s+=(count+'0');
+                    s+=(ans[i]);
+                }
+                ans=s;
+            }
+        
+        return ans;
+    }
+};
