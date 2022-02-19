fe00612a3991914f1dabe37f4201c97f52380843
Ayushi-Kosta <83204540+Ayushi-Kosta@users.noreply.github.com>
2022-02-20 01:10:39 +0530

    Time: 30 ms (20.29%), Space: 8.4 MB (15.40%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -2,31 +2,34 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         map<char, int> m1, m2;
-        int n1 = s1.size(), n2 = s2.size();
+        int n1=s1.size(), n2=s2.size();
         if(n1>n2){
             return 0;
         }
-        for(int i=0; i<n1; i++){
-            m1[s1[i]]++;
-            m2[s2[i]]++;
-        }
-        int i=0, j=n1;
-        while(j<n2){
-            if(m1==m2){
-                return 1;
+        else{
+            for(int i=0; i<n1; i++){
+                m1[s1[i]]++;
+                m2[s2[i]]++;
             }
-            else{
+            int i=0, j=n1;
+        
+            while(j<n2){
+                if(m1==m2){
+                    return 1;
+                }
+                m2[s2[j++]]++;
                 m2[s2[i]]--;
-                if(m2[s2[i]] == 0){
+                if(m2[s2[i]]==0){
                     m2.erase(s2[i]);
                 }
                 i++;
-                m2[s2[j++]]++;
-            }            
+                
+            }
+            if(m1==m2){
+                    return 1;
+                }
+            return 0;
+            
         }
-        if(m1==m2){
-            return 1;
-        }
-        return 0;
     }
 };
