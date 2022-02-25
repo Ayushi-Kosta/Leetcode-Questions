edb4a210fdd3a1b46f888f866cba63298c0893df
Ayushi-Kosta <83204540+Ayushi-Kosta@users.noreply.github.com>
2022-02-25 22:55:12 +0530

    Time: 8 ms (98.15%), Space: 18.8 MB (31.29%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void s(vector<int> &v, int n, int k, int i){
+        if(i>n) return ;
+        if(v.size() + n-(i) >=k) s(v, n, k, i+1);
+        v.push_back(i);
+        if(v.size() + n-(i) >=k) s(v, n, k, i+1);
+        if(v.size()==k){
+            ans.push_back(v);
+        }
+        v.pop_back();
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> v;
+        s(v, n, k, 1);
+        return ans;
+    }
+};
