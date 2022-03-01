eb71c3e3f405d841e6cbe3db2d7b8931817a27fa
Ayushi-Kosta <83204540+Ayushi-Kosta@users.noreply.github.com>
2022-03-01 13:50:32 +0530

    Time: 8 ms (52.50%), Space: 7.9 MB (48.88%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans, t(n+1);
+        t[0]=0;
+        for(int i=1; i<=n; i++){
+            t[i] = t[i/2] + (i%2);
+        }
+        return t;
+    }
+};
