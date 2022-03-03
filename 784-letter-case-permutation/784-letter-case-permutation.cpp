800ed04cab189520cd0052748b1f5c7f2b40dc4b
Ayushi-Kosta <83204540+Ayushi-Kosta@users.noreply.github.com>
2022-03-03 23:30:05 +0530

    Time: 0 ms (100.00%), Space: 10.8 MB (63.60%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.cpp
+++ 784-letter-case-permutation/784-letter-case-permutation.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
-    set<string> a;
+    vector<string> a;
     void st(string s, int i){
         if(i==s.size()){
-            a.insert(s);
+            // a.insert(s);
+            a.push_back(s);
             return;
         }  
         st(s, i+1);
-        s[i]=toupper(s[i]);
-        st(s, i+1);
+        if(s[i]>='a' and s[i]<='z'){
+            s[i]=toupper(s[i]);
+            st(s, i+1);    
+        }
+        
         
     }
     vector<string> letterCasePermutation(string s) {
@@ -16,13 +20,14 @@ public:
         for(int i=0; i<n; i++){
             s[i] = tolower(s[i]);
         }
-        a.insert(s);
+        // a.insert(s);
+        // a.push_back(s);
         st(s, 0);
-        vector<string> v;
-        for(auto &c: a){
-            v.push_back(c);
-        }
+        // vector<string> v;
+        // for(auto &c: a){
+        //     v.push_back(c);
+        // }
        
-        return v;
+        return a;
     }
 };
