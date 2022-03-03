4112e58538062854a2a41051bce4760a1e79c7ff
Ayushi-Kosta <83204540+Ayushi-Kosta@users.noreply.github.com>
2022-03-04 00:07:59 +0530

    Time: 15 ms (5.33%), Space: 7.4 MB (55.42%) - LeetHub

---
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numberOfArithmeticSlices(vector<int>& nums) {
+        int n = nums.size();
+        if(n<3){
+            return 0;
+        }
+        vector<int> dp(n, 0);
+        if(nums[1]-nums[0] == nums[2]-nums[1]) dp[2]=1;
+        int result=dp[2];
+        for(int i=3; i<n; i++){
+            if(nums[i]-nums[i-1] == nums[i-1]-nums[i-2]) dp[i]=dp[i-1]+1;
+            result+=dp[i];
+            cout<<dp[i]<<" ";
+        }
+        return result;
+    }
+};
