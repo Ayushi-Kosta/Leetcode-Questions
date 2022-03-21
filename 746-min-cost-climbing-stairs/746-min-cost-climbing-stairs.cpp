d6a1f46fcce34c8daa8ed4947224ace8057c55dc
Ayushi-Kosta <83204540+Ayushi-Kosta@users.noreply.github.com>
2022-03-22 01:00:01 +0530

    Time: 12 ms (36.14%), Space: 13.7 MB (50.55%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -4,6 +4,7 @@ public:
         int n = cost.size();
         for(int i=n-3; i>=0; i--){
             cost[i]=cost[i]+min(cost[i+1], cost[i+2]);
+            
         }
         return min(cost[0], cost[1]);
     }
