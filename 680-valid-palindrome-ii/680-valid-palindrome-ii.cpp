3342701de741adc0cdb9285db420fa783c85fbc6
Ayushi-Kosta <83204540+Ayushi-Kosta@users.noreply.github.com>
2022-04-03 10:25:12 +0530

    Time: 57 ms (85.97%), Space: 24 MB (31.98%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -1,53 +1,28 @@
 class Solution {
 public:
-    // bool validPalindrome(string s) {
-    //     int i=0, j = s.size()-1;
-    //     bool t=1;
-    //     while(i<j){
-    //         if(t and s[i]!=s[j]){
-    //             if(s[i+1]==s[j]){
-    //                 i++;
-    //                 t=0;
-    //             }
-    //             else if(s[j-1]==s[i]){
-    //                 j--;
-    //                 t=0;
-    //             }
-    //             else{
-    //                 return 0;
-    //             }
-    //         }
-    //         else if(s[i]==s[j]){
-    //             i++;
-    //             j--;
-    //         }
-    //         else if(!t){
-    //             return 0;
-    //         }
-    //     }
-    //     return 1;
-    // }
-    bool validPalindrome(string s) {
-        int i = 0;
-        int j = s.size() - 1;
-        
-        while(i <= j){
-            if(s[i] == s[j]){
+    bool ispalindrome(string s, int i, int j, int n){
+        while(i<j){
+            if(s[i]!=s[j]){
+                return 0;
+            }
+            else{
                 i++;
                 j--;
             }
-            else return isPalindrome(s, i + 1, j) || isPalindrome(s, i, j - 1);
         }
-        return true;
+        return 1;
     }
-    bool isPalindrome(string s, int i, int j){
-        while(i <= j){
-            if(s[i] == s[j]){
+    bool validPalindrome(string s) {
+        int n=s.size(), i=0, j=n-1;
+        while(i<j){
+            if(s[i]==s[j]){
                 i++;
                 j--;
             }
-            else return false;
+            else{
+                return ispalindrome(s, i+1, j, n) || ispalindrome(s, i, j-1, n);
+            }
         }
-        return true;
+        return 1;
     }
 };
