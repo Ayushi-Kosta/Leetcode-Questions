8699003dede02c8f250a1af3c6854232859fc605
Ayushi-Kosta <83204540+Ayushi-Kosta@users.noreply.github.com>
2022-04-10 01:08:58 +0530

    Time: 35 ms (12.82%), Space: 13.7 MB (41.62%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -4,13 +4,14 @@ public:
         vector<int> p;
         vector<pair<int, int>> w;
         int n = nums.size();
-        priority_queue<pair<int, int>> o;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int,int>>> o;
         map<int, int> m;
         for(int i=0; i<n; i++){
             m[nums[i]]++;
         }
         for(auto &x: m){
             o.push({x.second, x.first});
+            if(o.size()>k) o.pop();
         }
         int i=k;
         while(i--){
@@ -19,4 +20,20 @@ public:
         }
         return p;
     }
+    
+    // vector<int> topKFrequent(vector<int>& nums, int k) {
+    //     unordered_map<int, int> mp;
+    //     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    //     for(auto &i: nums) mp[i]++;
+    //     for(auto &i: mp) {
+    //         pq.push({i.second, i.first});
+    //         if(pq.size()>k) pq.pop();
+    //     }
+    //     vector<int> ans;
+    //     while(!pq.empty()) {
+    //         ans.push_back(pq.top().second);
+    //         pq.pop();
+    //     }
+    //     return ans;
+    // }
 };
