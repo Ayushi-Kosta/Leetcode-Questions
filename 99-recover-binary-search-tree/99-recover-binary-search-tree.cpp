07ba0e6097d3d5d5137aa56c9155a31166aec773
Ayushi-Kosta <83204540+Ayushi-Kosta@users.noreply.github.com>
2022-04-20 01:16:29 +0530

    Time: 62 ms (21.02%), Space: 57.8 MB (70.43%) - LeetHub

---
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* prev = NULL, *first=NULL, *second=NULL;
+    void inor(TreeNode* root){
+        if(root==NULL){
+            return ;
+        }
+        inor(root->left);
+        if(prev!=NULL and first==NULL){
+            if(prev->val>root->val){
+                first=prev;
+            }
+        }
+        // if(first!=NULL){
+            if(prev!=NULL and prev->val>root->val){
+                second=root;
+                // return;
+            } 
+        // }
+        prev=root;
+        inor(root->right);
+    }
+    void recoverTree(TreeNode* root) {
+        inor(root);
+        int s=0;
+        if(first!=NULL) s = first->val;
+        if(first!=NULL and second!=NULL) first->val = second->val;
+        if(second!=NULL) second->val = s;        
+    }
+};
