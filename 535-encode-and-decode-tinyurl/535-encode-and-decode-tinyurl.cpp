c2939ca35fe6485d2c79fbaceba48c1c8ed1f1fa
Ayushi-Kosta <83204540+Ayushi-Kosta@users.noreply.github.com>
2022-04-24 00:02:35 +0530

    Time: 6 ms (44.57%), Space: 6.8 MB (66.28%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
-
+    map<string, string> m;
+    long long a = 1;
+    string s = to_string(a);
+    
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
-        return longUrl;
+        m[s]=longUrl;
+        a++;
+        string ans=s;
+        s = to_string(a);
+        return ans;
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        return shortUrl;
+        return m[shortUrl];
     }
 };
 
