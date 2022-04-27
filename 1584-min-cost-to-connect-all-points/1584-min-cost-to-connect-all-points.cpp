80443f618438e23cb5e4b62b25de79e66b7bb1a9
Ayushi-Kosta <83204540+Ayushi-Kosta@users.noreply.github.com>
2022-04-27 09:46:35 +0530

    Time: 132 ms (92.60%), Space: 10.2 MB (93.38%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -57,24 +57,55 @@ public:
 //     }
     
     
+    // int minCostConnectPoints(vector<vector<int>>& points) {
+    //     int n = points.size();
+    //     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; //weight, vertex  - min heap
+    //     pq.push({0,0});
+    //     //we maintain MST set so that no vertex is counted twice and frms a cycle
+    //     vector<bool> visited(n, false);
+    //     int mst_vertex=0, weight=0;
+    //     while(mst_vertex<n){
+    //         pair<int, int> w = pq.top(); pq.pop();
+    //         int curr = w.second;
+    //         if(!visited[curr]){
+    //             weight+=(w.first);
+    //             visited[w.second]=true;
+    //             mst_vertex++;
+    //         }
+    //         for(int s=0; s<n; s++){
+    //             int w = abs(points[curr][0]-points[s][0]) + abs(points[curr][1]-points[s][1]);
+    //             if(visited[s]==false) pq.push({w, s});
+    //         }
+    //     }
+    //     return weight;
+    // }
+    
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n = points.size();
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; //weight, vertex  - min heap
-        pq.push({0,0});
+        vector<int> pq(n, INT_MAX); //weight, vertex  - min heap
         //we maintain MST set so that no vertex is counted twice and frms a cycle
+        pq[0]=0;
         vector<bool> visited(n, false);
         int mst_vertex=0, weight=0;
         while(mst_vertex<n){
-            pair<int, int> w = pq.top(); pq.pop();
-            int curr = w.second;
+            int curr_node=-1, curr_w=INT_MAX;
+            for(int i=0; i<n; i++){
+                if(visited[i]==false and curr_w>pq[i]){
+                    curr_w=pq[i];
+                    curr_node=i;
+                }
+            }
+            int curr = curr_node;
             if(!visited[curr]){
-                weight+=(w.first);
-                visited[w.second]=true;
+                weight+=(curr_w);
+                visited[curr_node]=true;
                 mst_vertex++;
             }
             for(int s=0; s<n; s++){
                 int w = abs(points[curr][0]-points[s][0]) + abs(points[curr][1]-points[s][1]);
-                if(visited[s]==false) pq.push({w, s});
+                if(visited[s]==false) {
+                    pq[s]=min(pq[s], w);
+                }
             }
         }
         return weight;
