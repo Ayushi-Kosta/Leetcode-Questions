67acbef3d3f4c0a0513dd05049f168ad5176801b
Ayushi-Kosta <83204540+Ayushi-Kosta@users.noreply.github.com>
2022-05-01 10:45:18 +0530

    Time: 1593 ms (33.33%), Space: 230.2 MB (33.33%) - LeetHub

--- 2261-k-divisible-elements-subarrays/2261-k-divisible-elements-subarrays.cpp
+++ 2261-k-divisible-elements-subarrays/2261-k-divisible-elements-subarrays.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:        
+    // int countDistinct(vector<int>& nums, int k, int p) {
+    //     int n = nums.size();
+    //     set<vector<int>> s;
+    //     for(int i=0; i<n; i++){
+    //         int c=0;
+    //         vector<int> v;
+    //         for(int j=i; j<n; j++){
+    //             v.push_back(nums[j]);
+    //             if(nums[j]%p==0){
+    //                 c++;
+    //             }
+    //             if(c>k){
+    //                 break;
+    //             }
+    //             s.insert(v);
+    //         }
+    //     }
+    //     return s.size();
+    // }
     int countDistinct(vector<int>& nums, int k, int p) {
+        set<vector<int>> ans;
         int n = nums.size();
-        set<vector<int>> s;
-        for(int i=0; i<n; i++){
-            int c=0;
-            vector<int> v;
-            for(int j=i; j<n; j++){
-                v.push_back(nums[j]);
-                if(nums[j]%p==0){
-                    c++;
-                }
-                if(c>k){
-                    break;
-                }
-                s.insert(v);
+        for(int i = 0; i<n; i++) {
+            vector<int>subarr;
+            int count = 0;
+            for(int j = i; j<n; j++) {
+                subarr.push_back(nums[j]);
+                count += (nums[j]%p == 0);
+                if(count <= k) ans.insert(subarr);
+                else break;
             }
         }
-        return s.size();
+        return ans.size();
     }
 };
