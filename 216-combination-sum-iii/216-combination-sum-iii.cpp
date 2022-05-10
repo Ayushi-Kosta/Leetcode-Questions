cedbed1ff38edd2f897bd932314148becc8cad07
Ayushi-Kosta <83204540+Ayushi-Kosta@users.noreply.github.com>
2022-05-10 07:17:13 +0530

    Time: 10 ms (5.87%), Space: 7.4 MB (11.07%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    void s(vector<int>& candidates, int target, vector<int> a, vector<vector<int>> &v, int         var, int k){
-        if(target==0 and k==0){
+    void s(vector<vector<int>> &v, vector<int> a, vector<int> b, int k, int n, int iterator){
+        int n_b = b.size();
+        if(n==0 and k==0){
             v.push_back(a);
-            return ;
+            return;
         }
-        if(target<0){
-            return ;
+        if(n<0){
+            return;
         }
-        int n = candidates.size();
-        for(int i=var; i<n; i++){
-            a.push_back(candidates[i]);
-            s(candidates, target-candidates[i], a, v, i+1, k-1);
+        for(int i=iterator; i<n_b; i++){
+            a.push_back(b[i]);
+            s(v, a, b, k-1, n-b[i], i+1);
             a.pop_back();
         }
+        
     }
-    
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int> z = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         vector<vector<int>> v;
-        vector<int> a;
-        s(z, n, a, v, 0, k);
+        vector<int> a, b={1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int iterator=0;
+        s(v, a, b, k, n, iterator);
         return v;
     }
 };
