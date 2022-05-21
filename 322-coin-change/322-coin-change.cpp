a7e8a81013eaed2b1b883d7967393571c201aa7a
Ayushi-Kosta <83204540+Ayushi-Kosta@users.noreply.github.com>
2022-05-22 01:36:09 +0530

    Time: 353 ms (7.46%), Space: 89 MB (5.12%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
+    int s(vector<vector<int>> &dp, vector<int>& coins, int amount, int i){
+        if(i<0 || amount<0){
+            return 1e7;
+        }
+        if(amount==0){
+            return 0;
+        }
+        if(dp[amount][i] != -1){
+            return dp[amount][i];
+        }
+        return dp[amount][i] = min(1+s(dp, coins, amount-coins[i], i), s(dp, coins, amount, i-1));
+    }
     int coinChange(vector<int>& coins, int amount) {
-        long long dp[amount+1];
-        dp[0]=0;
-        long long n_c = coins.size();
-        for(int i=1; i<amount+1; i++){
-            dp[i]=INT_MAX;
-            for(int j=0; j<n_c; j++){
-                if(i>=coins[j]){
-                    long long r = dp[i-coins[j]];
-                    dp[i] = min(r+1, dp[i]);
-                }
-            }            
-        }
-        if(dp[amount]>=INT_MAX){
-            return -1;
-        }
-        //cout<<(INT_MAX);
-        return dp[amount];
+        int n = coins.size();
+        vector<vector<int>> dp(amount+1, vector<int>(n, -1));
+        // row->amount, column->denomination        
+        int a = s(dp, coins, amount, n-1);
+        if(a==1e7) return -1;
+        return a;
     }
 };
