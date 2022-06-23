f8786c5afe0e8aa3d7d9f63de0be17ae3342b794
Ayushi-Kosta <83204540+Ayushi-Kosta@users.noreply.github.com>
2022-06-23 23:58:57 +0530

    Time: 6 ms (83.23%), Space: 6.6 MB (55.92%) - LeetHub

--- 38-count-and-say/38-count-and-say.cpp
+++ 38-count-and-say/38-count-and-say.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
-    string dp[31];
-    void rec(int n){
-        if(n==0){
+//     string dp[31];
+//     void rec(int n){
+//         if(n==0){
             
-            return;
-        }
-        if(n==1){
-            dp[1]="1";
-            return ;
-        }
-        if(n==2){
-            dp[2]="11";
-            return;
-        }
-        else{
-            rec(n-1);
+//             return;
+//         }
+//         if(n==1){
+//             dp[1]="1";
+//             return ;
+//         }
+//         if(n==2){
+//             dp[2]="11";
+//             return;
+//         }
+//         else{
+//             rec(n-1);
             
-            string s="";
-        int a=dp[n-1].size(), count=1, i=1;
-        while(i<a){
-            while(dp[n-1][i-1]==dp[n-1][i]){
-                count++;
-                i++;
-            }
-            s+=(count+'0');
-            s+=(dp[n-1][i-1]);
-            count=0;
-        }
-        dp[n]=s;    
+//             string s="";
+//         int a=dp[n-1].size(), count=1, i=1;
+//         while(i<a){
+//             while(dp[n-1][i-1]==dp[n-1][i]){
+//                 count++;
+//                 i++;
+//             }
+//             s+=(count+'0');
+//             s+=(dp[n-1][i-1]);
+//             count=0;
+//         }
+//         dp[n]=s;    
             
-        }
+//         }
         
         
-    }
+//     }
     string countAndSay(int n) {
         string ans="1";
         
