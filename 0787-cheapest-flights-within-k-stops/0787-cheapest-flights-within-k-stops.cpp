ab29ab4b9bd69ac264c8ed7593625f8e2b20ede8
Ayushi-Kosta <83204540+Ayushi-Kosta@users.noreply.github.com>
2023-01-26 17:23:10 +0530

    Time: 402 ms (5.04%), Space: 49.7 MB (5.78%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
+        /* distance from source to all other nodes */
+        vector<int> dist( n, INT_MAX );
+        dist[src] = 0;
+        
+        // Run only K+1 times since we want shortest distance in K hops
+        for( int i=0; i <= K; i++ ) {
+            vector<int> tmp( dist );
+            for( auto flight : flights ) {
+                if( dist[ flight[0] ] != INT_MAX ) {
+                    tmp[ flight[1] ] = min( tmp[flight[1]], dist[ flight[0] ] + flight[2] );
+                }
+            }
+            dist = tmp;
+        }
+        return dist[dst] == INT_MAX ? -1 : dist[dst];
+    }
+};
