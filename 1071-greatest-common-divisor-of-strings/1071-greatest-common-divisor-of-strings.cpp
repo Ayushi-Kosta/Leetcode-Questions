303a0c049c3965b715ebc5383153e28b2ae0d0ff
Ayushi-Kosta <83204540+Ayushi-Kosta@users.noreply.github.com>
2023-02-02 01:18:07 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (43.01%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        int n1=str1.size(), n2=str2.size();
+        if(str1+str2 == str2+str1) return str1.substr(0, gcd(n1, n2));
+        else return "";
+    }
+};
